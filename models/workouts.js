f8a8e4a0f2f25ea9094500b1df393e795951b496
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-07 20:45:45 -0500

    reformat file

--- models/workouts.js
+++ models/workouts.js
@@ -3,38 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  exercises: {
-    type: {
-        type: String
+    day: {
+        type: Date,
+        default: Date.now
     },
-    name: {
-        type: String
-    },
-    weight: {
-        type: Number,
-        default: 0
-    },
-    sets: {
-        type: Number,
-        default: 0
-    },
-    reps: {
-        type: Number,
-        default: 0
-    },
-    duration: {
-        type: Number,
-        default: 0
-    },
-    distance: {
-        type: Number,
-        default: 0
+    exercises: {
+        name: {
+            type: String
+        },
+        type: {
+            type: String
+        },
+        weight: {
+            type: Number,
+            default: 0
+        },
+        sets: {
+            type: Number,
+            default: 0
+        },
+        reps: {
+            type: Number,
+            default: 0
+        },
+        duration: {
+            type: Number,
+            default: 0
+        },
+        distance: {
+            type: Number,
+            default: 0
+        }
     }
-  }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
