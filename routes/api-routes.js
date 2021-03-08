e52621574bec134a1d64d6e7f6159c2538547563
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-07 20:47:49 -0500

    added get route for dashboard

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,29 +3,39 @@ const Workout = require("../models/workouts.js");
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-        .sort({date: -1})
+        .sort({ date: -1 })
         .then(dbWorkout => {
             res.json(dbWorkout)
         })
         .catch(err => {
             res.status(400).json(err);
-        });
-})
+        })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
 
 router.post("api/workout/:id", (req, res) => {
     Workout.create(req.body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
 
 });
 
 router.put("api/workouts/:id", (req, res) => {
     const id = req.params.id;
-    Workout.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: req.body}}, {new: true})
+    Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
